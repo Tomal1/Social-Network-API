150cc13ae8ec4923174ec1836e1dc8ed8baec31b
Minhazul Islam <minhazulislam@hotmail.com>
2023-02-13 15:42:42 +0000

    get route has now been built in server.js

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const { userThought } = require("./models");
 const app = express()
 const PORT = 3001;
 
-
-
-
-
-
-
-
+app.get("/", (req, res) => {
+  // Using model in route to find all documents that are instances of that model
+  userThought.find({}, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {

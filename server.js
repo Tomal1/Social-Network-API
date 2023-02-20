f49a18407ba2c4669285e89591f0155ea3a0aa0a
Minhazul Islam <minhazulislam@hotmail.com>
2023-02-20 18:22:57 +0000

    update route added but was not working because middlewere was not established in server.js

--- server.js
+++ server.js
@@ -4,8 +4,48 @@ const { userThought } = require("./models");
 const app = express()
 const PORT = 3001;
 
+// need this middlewhere else dosent render body during update
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.post("/", (req, res) => {
+  const newThought = new userThought(
+    { 
+      user: req.params.user, 
+      thoughts: req.params.thoughts 
+   });
+   newThought.save();
+  if (newThought) {
+    res.status(200).json(newThought);
+  } else {
+    console.log('Uh Oh, something went wrong');
+    res.status(500).json({ message: 'something went wrong' });
+  }
+});
+
+app.post("/:_id", (req, res) => {
+  console.log(req.body)
+  userThought.findOneAndUpdate(
+    //targeting the desired id
+    {_id:req.params._id},
+    //items just disappear and nothing rendering
+    //req.body targets the body inside insomnia
+    req.body,
+    //needs this to render updated record
+    {new:true},
+    (err, result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        console.log('Uh Oh, something went wrong');
+        res.status(500).json({ message: 'something went wrong' });
+      }
+    }
+  )
+})
+
 app.get("/", (req, res) => {
-  // Using model in route to find all documents that are instances of that model
   userThought.find({}, (err, result) => {
     if (result) {
       res.status(200).json(result);
@@ -17,7 +57,6 @@ app.get("/", (req, res) => {
 });
 
 app.delete("/:_id", (req, res) => {
-  // Using model in route to find all documents that are instances of that model
   userThought.findOneAndDelete({ _id: req.params._id}, (err, result) => {
     if (result) {
       res.status(200).json(result);

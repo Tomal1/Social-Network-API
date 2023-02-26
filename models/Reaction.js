9a8b36bb8f3457f1290a4e4f8318a70ce0ab8e68
Minhazul Islam <minhazulislam@hotmail.com>
2023-02-26 13:12:08 +0000

    modifications to reactions schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -3,15 +3,37 @@ const mongoose = require('mongoose');
 
 const ReactionSchema = new mongoose.Schema({
     //how to Use Mongoose's ObjectId data type
-    reactionId: ObjectId,
-    //
-    reactionBody: { type: String, required: true, maxlength: 280 },
-    username: {type: String, required: true},
-    createdAt: {type: Date, default: Date.now }
-  });
+    reactionId:{
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    
+    reactionBody: { 
+        type: String, 
+        required: true, 
+        maxlength: 280 
+    },
+    username: {
+        type: String, 
+        required: true
+    },
+    createdAt: {
+        type: Date, 
+        default: Date.now 
+        get: (date) => {
+            return `${formatDate(date)} ${formatTime(date)}`
+        }
+    }
+  },
+  {
+    toJSON: {
+        getters: true,
+    },
+    id: false
+});
 
     // this is a model,
-    const Reaction = mongoose.model("Reaction", Reaction);
+    const Reaction = mongoose.model("Reaction", ReactionSchema);
 
 /////////
   module.exports = Reaction;
--- models/User.js
+++ models/User.js
@@ -1,25 +1,32 @@
 // Require schema and model from mongoose
 const mongoose = require('mongoose');
 
-const { Thoughts } = require("../models")
+const { Thoughts, User } = require("../models")
 
   /// this is the real one
   const UserSchema = new mongoose.Schema({
-    username: { type: String, Unique: true, required: true, trim:true },
-    email: {type: String, required: true, Unique: true},
-    
-    thoughts:[
-    {
-      type: Thoughts,
-      ref:"thoughts",
-
+    username: { 
+      type: String, 
+      Unique: true, 
+      required: true, 
+      trim:true 
     },
+    email: {
+      type: String, 
+      required: true, 
+      Unique: true
+    },
+    thoughts:[
+      {
+        type: Thoughts,
+        ref:"thoughts",
+      },
     ],
     friends:[
-    {
-      type: User,
-      ref:"User",
-    },
+      {
+        type: User,
+        ref:"User",
+      },
    ],
   },
   {
@@ -28,7 +35,6 @@ const { Thoughts } = require("../models")
     },
     id: false,
   });
-
 
 //this is the model
   const User = mongoose.model("User", UserSchema);

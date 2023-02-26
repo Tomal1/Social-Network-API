5f067727858bde8299eca27853ae3a7a479ce589
Minhazul Islam <minhazulislam@hotmail.com>
2023-02-26 13:22:23 +0000

    modifications to Thoughts schema previous commit was for User not thoughts

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -3,17 +3,37 @@ const mongoose = require('mongoose');
 
 const { Reaction } = require("./Reaction");
 
+const { formatDate, formatTime } = require('../utils/dateFormat');
+
 const ThoughtSchema = new mongoose.Schema({
-    thoughtText: {type: String, required: true, minlength: 1, maxlength: 280 },
-    createdAt: {type: Date, default: Date.now },
-    username: {type: String, required: true},
-    reactions:[
-      {
-        type: Reaction,
-        ref:"Reactions",
-      },
-     ],
-  });
+    thoughtText: {
+        type: String, 
+        required: true, 
+        minlength: 1, 
+        maxlength: 280 
+    },
+    createdAt: {
+        type: Date, 
+        default: Date.now 
+    },
+    username: {
+        type: String, 
+        required: true
+    },
+    reactions:[Reaction],
+  },
+  {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false
+    }
+  );
+
+  ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length
+});
 
   const Thoughts = mongoose.model("Thoughts", ThoughtSchema);
 

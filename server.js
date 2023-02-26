87af4835ea7ece7dc37d316b20697c453a576b94
Minhazul Islam <minhazulislam@hotmail.com>
2023-02-26 21:21:20 +0000

    CRUD for User schema has now been tested and fully operating

--- server.js
+++ server.js
@@ -26,26 +26,50 @@ app.use(express.json());
 //   }
 // });
 
-// app.post("/:_id", (req, res) => {
-//   console.log(req.body)
-//   userThought.findOneAndUpdate(
-//     //targeting the desired id
-//     {_id:req.params._id},
-//     //items just disappear and nothing rendering
-//     //req.body targets the body inside insomnia
-//     req.body,
-//     //needs this to render updated record
-//     {new:true},
-//     (err, result) => {
-//       if (result) {
-//         res.status(200).json(result);
-//       } else {
-//         console.log('Uh Oh, something went wrong');
-//         res.status(500).json({ message: 'something went wrong' });
-//       }
-//     }
-//   )
-// })
+app.post("/:_id", (req, res) => {
+  console.log(req.body)
+  userThought.findOneAndUpdate(
+    //targeting the desired id
+    {_id:req.params._id},
+    //items just disappear and nothing rendering
+    //req.body targets the body inside insomnia
+    req.body,
+    //needs this to render updated record
+    {new:true},
+    (err, result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        console.log('Uh Oh, something went wrong');
+        res.status(500).json({ message: 'something went wrong' });
+      }
+    }
+  )
+})
+
+app.post("/User",(req,res)=>{
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+  })
+  .then(data=>res.status(200).json(data))
+  .catch(err=>res.status(500).json(err))
+})
+
+app.put("/User/:_id", (req,res)=>{
+  User.findOneAndUpdate(
+    //targeting the desired id
+    {_id:req.params._id},
+    //items just disappear and nothing rendering
+    //req.body targets the body inside insomnia
+    req.body,
+    //needs this to render updated record
+    {new:true},
+  )
+  .then(data=>res.status(200).json(data))
+  .catch(err=>res.status(500).json(err))
+})
+
 
 app.get("/User", (req, res)=>{
   User.find({})
@@ -53,7 +77,7 @@ app.get("/User", (req, res)=>{
   .catch(err=>res.status(500).json(err))
 })
 
-app.get("/User:_id", (req,res)=>{
+app.get("/User/:_id", (req,res)=>{
   User.findOne({
     _id:req.params._id,
   })
@@ -61,8 +85,17 @@ app.get("/User:_id", (req,res)=>{
   .catch(err=>res.status(500).json(err))
 })
 
+app.delete("/User/:_id",(req,res)=>{
+  User.deleteOne({
+    _id:req.params._id,
+  })
+  .then(data=>res.status(200).json(data))
+  .catch(err=>res.status(500).json(err))
+})
+
+
 app.delete("User:_id",(req,res)=>{
-  User.findOneAndDelete({
+  Thoughts.findOneAndDelete({
     _id:req.params._id,
   })
   .then(data=>res.status(200).json(data))
@@ -75,15 +108,6 @@ app.get("/Thoughts", (req, res)=>{
   .then(data=>res.status(200).json(data))
   .catch(err=>res.status(500).json(err))
 })
-
-app.delete("User:_id",(req,res)=>{
-  Thoughts.findOneAndDelete({
-    _id:req.params._id,
-  })
-  .then(data=>res.status(200).json(data))
-  .catch(err=>res.status(500).json(err))
-})
-
 
 
 
